8f5e33cffcb091b247969e8f223c7e35e2cecd82
Nicolas Basty <nic.basty@gmail.com>
2019-10-16 00:42:11 +0100

    Update scimat_make3dframe.m

--- scimat_make3dframe.m
+++ scimat_make3dframe.m
@@ -1,11 +1,14 @@
+% Author: Nicolas Basty 
+
 function scimat = scimat_make3dframe(scimat2d,factor)
-% SCIMAT_MAKE3DFRAME Generates an empty 3D scimat grid from a 2D scimat input (single image or stack) for
-% use with the scimat_insert_extract.m function. The resolution will depend on the "factor" input and
-% z is the resolution in the z axis in the 2D image
+   % SCIMAT_MAKE3DFRAME Generates an empty 3D scimat grid from a 2D scimat input (single image or stack) for
+   % use with the scimat_insert_extract.m function. The resolution will depend on the "factor" input and
+   % z is the resolution in the z axis in the 2D image
 
 imsize1 = size(scimat2d(1).data,1);
 imsize2 = size(scimat2d(1).data,2);
-xi = zeros(imsize1,imsize2,numel(scimat2d));% initialise arrays storing the real world coordinates of each pixel of each slice
+xi = zeros(imsize1,imsize2,numel(scimat2d));
+   % initialise arrays storing the real world coordinates of each pixel of each slice
 yi = xi;
 zi = xi;
 
@@ -19,11 +22,11 @@ for sn = 1:numel(scimat2d)
    
 end
 
-% the ranges of xyz in real world coordinates of all images stacked together
+   % the ranges of xyz in real world coordinates of all images stacked together
 rangex = [min(xi(:)) max(xi(:))];
 rangey = [min(yi(:)) max(yi(:))];
 rangez = [min(zi(:)) max(zi(:))];
-% vectors with real world coordinates and stepsize by factor is the resolution
+   % vectors with real world coordinates and stepsize by factor is the resolution
 xRW = rangex(1):x(end)/factor:rangex(2);
 yRW = rangey(1):y(end)/factor:rangey(2);
 zRW = rangez(1):z(end)/factor:rangez(2);
